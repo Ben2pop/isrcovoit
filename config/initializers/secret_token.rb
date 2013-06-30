048052dde3ce23c8b98785b8c647f6d1b098b2ac
ben2pop <r.bendenoun@gmail.com>
2013-07-01 00:24:06 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isrcovoit::Application.config.secret_token = '600ca3e273dd22ba74348c4edd33f9a510a631d1aaab9b92119697ef82049a53c5af7af28c3dc6442f7f1c20fca849ae96d4812ca69a3e4778ef00640b59a856'
